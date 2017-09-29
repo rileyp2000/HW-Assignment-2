1f1ec9de66682c53cad53cbd4938f2043b69b863
rileyp2000 <rileyp@bishopireton.org>
2017-09-29 12:17:33 -0400

    comments

--- HW Assign 2/src/RankComparator.java
+++ HW Assign 2/src/RankComparator.java
@@ -1,5 +1,9 @@
 import java.util.Comparator;
-
+/**
+ * <p>This is a rank comparator </p>
+ * @author rileyp
+ *
+ */
 public class RankComparator implements Comparator<Card> {
 
 	private boolean ascending;
@@ -7,11 +11,22 @@ public class RankComparator implements Comparator<Card> {
 	public RankComparator() {
 		ascending = true;
 	}
-
+	
+	/**
+	 * 
+	 * @param a if order is ascending or not
+	 */
 	public RankComparator(boolean a) {
 		ascending = a;
 	}
-
+	
+	
+	/**
+	 * @param c1 A card
+	 * @param c2 another card
+	 * 
+	 * @return c1's rank - c2's rank
+	 */
 	@Override
 	public int compare(Card c1, Card c2) {
 		if (ascending)

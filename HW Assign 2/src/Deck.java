614f74efe04343ab62318673da163909b7259131
rileyp2000 <rileyp@bishopireton.org>
2017-10-02 14:05:19 -0400

    finsihed shuffle and tostring. but shuffle goofs it

--- HW Assign 2/src/Deck.java
+++ HW Assign 2/src/Deck.java
@@ -1,6 +1,9 @@
 /**
- * <p>This class represents a deck of card</p> 
+ * <p>
+ * This class represents a deck of card
+ * </p>
  * Sep 29, 2017
+ * 
  * @author Patrick Riley
  */
 
@@ -8,42 +11,92 @@ public class Deck {
 	private Card[] cards;
 	private int topCard;
 	private boolean sorted;
-	
-	
+
+	private static final int NUMSUITS = (Card.getSUITS()).length;
+	private static final int NUMCARDS = (Card.getRANKS()).length;
+	private static final int TOTALCARDS = NUMSUITS * NUMCARDS;
+
 	public Deck() {
 		sorted = true;
 		cards = createDeck(sorted);
-		topCard = cards.length -1;
-		
+		topCard = cards.length - 1;
+
 	}
-	
+
 	public Deck(boolean s) {
 		cards = createDeck(s);
-		topCard = cards.length -1;
+		topCard = cards.length - 1;
 		sorted = s;
 	}
 
-	private Card[] createDeck(boolean sorted) { //FIX THIS!@!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		
+	public void setCards(Card[] c) {
+		cards = c;
+	}
+
+	private Card[] createDeck(boolean sorted) { // FIX THIS!@!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 		Card[] cards = new Card[52];
 		String[] suits = Card.getSUITS();
 		String[] ranks = Card.getRANKS();
 		int cardCounter = 0;
-		
-		for(int i = 0; i < Card.getSUITS().length; i ++) {
-			for(int j = 9; j < Card.getRANKS().length; j++) {
+
+		for (int i = 0; i < NUMSUITS; i++) {
+			for (int j = 0; j < NUMCARDS; j++) {
 				cards[cardCounter] = (new Card(suits[i], ranks[j]));
 				cardCounter++;
 			}
 		}
-		
-		if(sorted)
-			return cards;
-		else
-			return shuffle(cards);
-				
-				
+
+		if (!sorted)
+			shuffle();
+
+		return cards;
+
 	}
-	
-	
+
+	public void shuffle() {
+		Card[] replace = new Card[TOTALCARDS];
+
+		for (int i = 0; i < replace.length - 1; i++) {
+			int rand = (int) (Math.random() * TOTALCARDS);
+			if (cards[rand] != null)
+				replace[i] = cards[rand];
+			cards[rand] = null;
+
+		}
+		cards = replace;
+
+	}
+
+	public String toString() {
+		String result = "";
+		if (topCard == NUMCARDS - 1) {
+			for (int s = 0; s < NUMCARDS; s += NUMSUITS) {
+				for (int r = 0; r < NUMSUITS; r++) {
+					result += cards[NUMCARDS * s + r] + "\t";
+				}
+				result += "\n";
+
+			}
+		}
+		else {
+			for(Card c : cards)
+				result += c.toString() + "\n";
+		}
+		return result;
+
+	}
+
+	public boolean equals(Deck d) {
+		if (cards.length != d.cards.length)
+			return false;
+
+		for (int i = 0; i < TOTALCARDS; i++) {
+			if (cards[i] != d.cards[i])
+				return false;
+		}
+
+		return true;
+	}
+
 }
---
+++ HW Assign 2/src/DeckTester.java
@@ -0,0 +1,24 @@
+/**
+ * <p></p> 
+ * Oct 2, 2017
+ * @author Patrick Riley
+ */
+
+/**
+ * @author rileyp
+ *
+ */
+public class DeckTester {
+
+	/**
+	 * @param args
+	 *
+	 *void
+	 */
+	public static void main(String[] args) {
+		Deck d = new Deck();
+		System.out.println(d);
+
+	}
+
+}

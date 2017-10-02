bb99c01242b4b0f9f65f870c1e7c26ad9b8f4e95
rileyp2000 <rileyp@bishopireton.org>
2017-10-02 14:05:32 -0400

    try decktester

--- HW Assign 2/src/DeckTester.java
+++ HW Assign 2/src/DeckTester.java
@@ -18,6 +18,8 @@ public class DeckTester {
 	public static void main(String[] args) {
 		Deck d = new Deck();
 		System.out.println(d);
+		d.shuffle();
+		System.out.println("\n\n\n\n" + d);
 
 	}
 
